a74bba6ab024408aebb3e6b8ae81731e6702e041
mehmetbatin <78997333+mehmetbatin@users.noreply.github.com>
2021-03-03 20:06:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-  host: 'minelingonetwork.aternos.me', // optional
-  port: 26302,       // optional
-  username: 'Minelingo_Bot', // email and password are required only for
-  version: false,                 // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
+  host: 'minelingonetwork.aternos.me',
+  port: 26302,
+  username: 'Minelingo_Bot',
+  version: false,
 })
 
 bot.on('chat', function (username, message) {
@@ -12,6 +12,6 @@ bot.on('chat', function (username, message) {
   bot.chat(message)
 })
 
-// Log errors and kick reasons:
+
 bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
 bot.on('error', err => console.log(err))
